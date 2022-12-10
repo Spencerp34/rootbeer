e6c8e965d4d12e3e8aa4f3d3b64445fcd52984f4
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-12-10 15:18:58 -0700

    Set up basic routing and buttons

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,5 +1,5 @@
 import AuthStyle from "./auth.style";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Auth = () => {
     const [loggedIn, setIsLoggedIn] = useState(false)
@@ -8,7 +8,9 @@ const Auth = () => {
         <section>
             <AuthStyle>
                 <h1>Login/Signup</h1>
-                <p>Logged in? {loggedIn ? "Yes" : "No"}</p>
+                <p>{loggedIn ? "Logged In" : "Logged Out"}</p>
+                <button onClick={() => setIsLoggedIn(true)} >Log In</button>
+                <button onClick={() => setIsLoggedIn(true)} >Sign Up</button>
             </AuthStyle>
         </section>
     )
