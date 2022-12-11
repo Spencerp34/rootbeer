5a8ad6b8685336a49d80ada613f843bb377f21ad
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-12-11 14:24:01 -0700

    added better layout for app and header

--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -2,11 +2,28 @@ import styled from "styled-components";
 
 const HeaderStyle = styled.div`
 
+    .bottom-header {
+        display: flex;
+        justify-content: end;
+    }
+
     .login {
-        width: 30%;
+        width: 20%;
         display: flex;
         justify-content: space-around;
-      }
+    }
+
+    .buttons {
+        display: flex;
+    }
+
+    .button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 5px;
+        text-align: center;
+    }
 `;
 
 export default HeaderStyle;
