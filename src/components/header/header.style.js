8425d8c9d0caba59c1b5e2ed6a9d90f3b5d9d072
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-12-11 15:07:37 -0700

    added some colors I like to the auth buttons

--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -7,6 +7,10 @@ const HeaderStyle = styled.div`
         justify-content: end;
     }
 
+    .logged-out {
+        display: flex;
+    }
+
     .login {
         width: 20%;
         display: flex;
@@ -21,8 +25,24 @@ const HeaderStyle = styled.div`
         display: flex;
         justify-content: center;
         align-items: center;
+        padding: 15px;
+        background-color: #5B3A0A;
+        color: #f2e1c1;
         margin-left: 5px;
+        border-radius: 5px;
         text-align: center;
+        text-decoration: none;
+    }
+
+    #login {
+        background-color: #f2e1c1;
+        color: #5B3A0A;
+        border: 2px solid #5B3A0A;
+    }
+    #signup {
+        background-color: #5B3A0A;
+        color: #f2e1c1;
+        border: 2px solid #5B3A0A;
     }
 `;
 
