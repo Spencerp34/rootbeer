3fcb07e895b7452b3b940c689effdd9b958fd79c
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-02 11:18:38 -0700

    Getting banners more properly sized

--- src/pages/home/home.style.js
+++ src/pages/home/home.style.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 const HomeStyle = styled.div`
-
+    width: 75vw;
+    height: 100vw;
+    margin-left: 2vw;
+    margin-right: 2vw;
+    background-color: white;
+    padding: 15px;
 `;
 
 export default HomeStyle;
