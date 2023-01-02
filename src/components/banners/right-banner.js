626b17588ef2fae5fb963dc0677390f6e5ecc452
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-02 11:25:34 -0700

    Separated left banner and right banner

---
+++ src/components/banners/right-banner.js
@@ -0,0 +1,13 @@
+import RightBannerStyle from "./right-banner.style";
+
+const RightBanner = () => {
+    return(
+        <section>
+            <RightBannerStyle>
+                
+            </RightBannerStyle>
+        </section>
+    )
+}
+
+export default RightBanner;
--- src/components/banners/right-banner.style.js
+++ src/components/banners/right-banner.style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const BannerStyle = styled.div`
+const RightBannerStyle = styled.div`
     background-image: url("https://media1.giphy.com/media/ZrlW0uM0tGurzdqc3A/giphy.gif?cid=ecf05e47639ro8ibr54aioen7f8ie6mc27kfisjvsbu37w72&rid=giphy.gif&ct=g");
     background-size: auto 100%;
     background-repeat: no-repeat;
@@ -10,4 +10,4 @@ const BannerStyle = styled.div`
     overflow: hidden;
 `
 
-export default BannerStyle;
+export default RightBannerStyle;
