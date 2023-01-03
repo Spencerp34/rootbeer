ef42aee439fe0f44b04aea14eb312dd823d0ee8f
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-03 12:51:03 -0700

    Got the routing set up so it will redirect when logged in and authed

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,10 +1,17 @@
 import AuthStyle from "./auth.style";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 const Auth = () => {
 
+    useEffect(() => {
+        if(window.localStorage.getItem("token")){
+            window.location = "/"
+        }
+    }, [])
+
     const handleLogIn = () => {
         window.localStorage.setItem("token", "Spencer Patterson")
+        window.location = "/"
     }
 
     return(
