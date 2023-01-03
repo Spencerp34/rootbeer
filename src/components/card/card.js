f06881c6f29b0e9d9d3fb219218a4dd1b9ab6f7a
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-03 09:58:40 -0700

    Added half star for future

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,6 +1,7 @@
 import CardStyle from "./card.style";
 import PARB1 from "../../assets/PARB1.png";
-import RatingImg from "../../assets/pixelartRB.png";
+import Star from "../../assets/Star.png";
+import HalfStar from "../../assets/HalfStar.png";
 
 const Card = () => {
     return(
@@ -9,16 +10,17 @@ const Card = () => {
                 <div className="ex">A&W</div>
                 <img src={PARB1} width={"60%"} alt="Root Beer Brand" />
                 <div className="Author Rating">
-                    <img src={RatingImg} width={50} alt="root beer rating" />
-                    <img src={RatingImg} width={50} alt="root beer rating" />
-                    <img src={RatingImg} width={50} alt="root beer rating" />
-                    <img src={RatingImg} width={50} alt="root beer rating" />
-                    <img src={RatingImg} width={50} alt="root beer rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={Star} width={50} alt="star rating" />
                 </div>
                 <div className="Consumer Rating">
-                    <img src={RatingImg} width={50} alt="root beer rating" />
-                    <img src={RatingImg} width={50} alt="root beer rating" />
-                    <img src={RatingImg} width={50} alt="root beer rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={Star} width={50} alt="star rating" />
+                    <img src={HalfStar} width={27} alt="half star rating" />
                 </div>
             </CardStyle>
         </section>
