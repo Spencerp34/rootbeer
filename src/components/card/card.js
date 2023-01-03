eda1644711120a905cf6ae5878d95a0ce7d8b3d1
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-03 14:52:18 -0700

    set up a backup picture for all the pixelarts

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,20 +1,32 @@
 import CardStyle from "./card.style";
-import PARB1 from "../../assets/PARB1.png";
 import Star from "../../assets/Star.png";
 import HalfStar from "../../assets/HalfStar.png";
+import { pixelArts } from "../../constants";
 
-const Card = () => {
+const Card = (props) => {
+
+    const {result, index} = props
+
+    let pic = Star;
+    if(result.pic){
+        pic = result.pic
+    }else{
+        console.log(index)
+        let i = index % pixelArts.length
+        pic = pixelArts[i]
+    }
+
     return(
         <section>
             <CardStyle>
                 <div className="ex">A&W</div>
-                <img src={PARB1} width={"60%"} alt="Root Beer Brand" />
+                <img src={pic} width={"60%"} alt="Root Beer Brand" />
                 <div className="Author Rating">
                     <img src={Star} width={50} alt="star rating" />
                     <img src={Star} width={50} alt="star rating" />
                     <img src={Star} width={50} alt="star rating" />
                     <img src={Star} width={50} alt="star rating" />
-                    <img src={Star} width={50} alt="star rating" />
+                    <img src={HalfStar} width={27} alt="star rating" />
                 </div>
                 {/* <div className="Consumer Rating">
                     <img src={Star} width={50} alt="star rating" />
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,33 +2,18 @@ import HomeStyle from "./home.style";
 import Card from "../../components/card/card";
 
 const Home = () => {
+    
+    const results = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+
     return(
         <section>
             <HomeStyle>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+
+                {results.map((result, index) => {
+                    return(
+                        <Card result={result} index={index} />
+                    )
+                })}
             </HomeStyle>
         </section>
     )
