882ca1a0820631d14dcc11f917e7d43dfbff171e
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2023-01-04 11:41:28 -0700

    added a rendering function for the rating for when it is dynamic

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -16,17 +16,21 @@ const Card = (props) => {
         pic = pixelArts[i]
     }
 
+    const ratingRender = (rating) => {
+        let stars = []
+        for(let i = 0; i < rating; i++){
+            stars.push(<img src={Star} width={50} alt="star rating" />)
+        }
+        return stars
+    }
+
     return(
         <section>
             <CardStyle>
                 <div className="ex">A&W</div>
                 <img src={pic} width={"60%"} alt="Root Beer Brand" />
                 <div className="Author Rating">
-                    <img src={Star} width={50} alt="star rating" />
-                    <img src={Star} width={50} alt="star rating" />
-                    <img src={Star} width={50} alt="star rating" />
-                    <img src={Star} width={50} alt="star rating" />
-                    <img src={HalfStar} width={27} alt="star rating" />
+                    {ratingRender(4)}
                 </div>
                 {/* <div className="Consumer Rating">
                     <img src={Star} width={50} alt="star rating" />
